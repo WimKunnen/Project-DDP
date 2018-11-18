a36f795f9acfe3a3dd76225d9d5a68f8715addfc
Wim Kunnen <wim.kunnen@student.kuleuven.be>
2018-11-19 00:17:37 +0100

    Improvements to exponentiation

--- actual_project/hw_project/project_hw/project_hw.srcs/sources_1/new/exponentiation.v
+++ actual_project/hw_project/project_hw/project_hw.srcs/sources_1/new/exponentiation.v
@@ -48,16 +48,12 @@ module exponentiation(
     reg [1023:0] reg_x;
     reg [1023:0] x_tilde;
     reg [1023:0] e;
-    reg [1023:0] m;
     reg [9:0] size;
     reg [9:0] counter;
     reg [2:0] state, nextstate;
-    reg step;
-    reg loop;
-    reg end_loop;
 
     assign mont_input_a = (state == 3'b1) ? reg_x  : {a};
-    assign mont_input_b = (loop == 0 )    ? ( (end_loop ==0) ? in_r2 : {1024'b1} ) : ( (state == 3'b4) ? {a} : {x_tilde} );
+    assign mont_input_b = (state == 3'b0 || state == 3'b5)    ? ( (state == 3'b5) ? r2 : {1024'b1} ) : ( (state == 3'b3) ? {a} : {x_tilde} );
 
     always @(posedge clk)
     begin
@@ -68,11 +64,13 @@ module exponentiation(
           a     <= in_r;
           e     <= in_e;
           t     <= in_t;
-          m     <= in_m;
           reg_x <= in_x;
         end
         3'b1:
-          x_tilde <= mont_result;
+        begin
+          mont_input_m <= in_m;
+          x_tilde      <= mont_result;
+        end
         3'b2:
           a <= mont_result;
         3'b3:
@@ -196,63 +194,47 @@ module exponentiation(
         3'b0:
           begin
             mont_resetn  <= 1'b0;
-            loop         <= 1'b0;
-            end_loop     <= 1'b0;
             count_enable <= 1'b0;
           end
         // X tilde state
         3'b1:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b0;
-            end_loop     <= 1'b0;
             count_enable <= 1'b0;
           end
         // Loop state 1
         3'b2:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b1;
-            end_loop     <= 1'b0;
             count_enable <= 1'b1;
           end
         // Loop state 2 e[t] = 1
         3'b3:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b1;
-            end_loop     <= 1'b0;
             count_enable <= 1'b0;
           end
         // Loop state 2 e[t] = 0
         3'b4:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b1;
-            end_loop     <= 1'b0;
             count_enable <= 1'b0;
           end
         // Last mont state
         3'b5:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b0;
-            end_loop     <= 1'b1;
             count_enable <= 1'b0;
           end
         // Done state
         3'b6:
           begin
             mont_resetn  <= 1'b1;
-            loop         <= 1'b0;
-            end_loop     <= 1'b1;
             count_enable <= 1'b0;
           end
         default :
           begin
             mont_resetn  <= 1'b0;
-            loop         <= 1'b0;
-            end_loop     <= 1'b0;
             count_enable <= 1'b0;
           end
       endcase
